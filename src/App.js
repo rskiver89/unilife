a248da2d0a14e0c7aaaa01d3c26d17073b9491da
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-02-22 23:08:53 -0500

    add footer contents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import './App.css';
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <ul className='footer-content'>
+        <li>About Us</li>
+        <li>Terms & Conditions</li>
+        <li>Privacy & Cookie Policies</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
