ec5608e880bddf6af22b3557f37debf188e8ae34
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-04-03 17:24:45 -0400

    finish HomeDetails page

--- src/components/BedroomPrices.js
+++ src/components/BedroomPrices.js
@@ -5,6 +5,7 @@ function BedroomPrices({ bedroomPrices }) {
   const bedrooms = Object.entries(bedroomPrices);
 
   return (
+    <div className='bedroom-prices-wrapper'>
     <div className="bedroom-prices-container">
       {bedrooms.map(([bedroom, price], index) => (
         <div key={index} className="bedroom-prices">
@@ -12,6 +13,7 @@ function BedroomPrices({ bedroomPrices }) {
           <p>${price}</p>
         </div>
       ))}
+    </div>
     </div>
   );
 };
