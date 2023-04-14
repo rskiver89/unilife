ecb1ee8e21e05188af59618460d320772e034ff7
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-04-13 22:14:10 -0400

    update ViewingModal styling

--- src/components/ViewingModal.js
+++ src/components/ViewingModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/ViewingModal.css';
+import { MdOutlineAddHomeWork } from 'react-icons/md';
 
 function ViewingModal({ isOpen, address, onClose }) {
   if (!isOpen) {
@@ -8,16 +9,44 @@ function ViewingModal({ isOpen, address, onClose }) {
 
   return (
     <div className="viewing-modal-backdrop">
+              <button onClick={onClose} className="close-button">X</button>
       <div className="viewing-modal">
-        <button onClick={onClose} className="close-button">X</button>
-        <h2>Book a Viewing</h2>
-        <p>{address}</p>
+        <div className='modal-header-container'>
+          <div className='modal-header'>
+            <h2 style={{marginBottom: '16px'}}>Book a Viewing</h2>
+            <address>{address}</address>
+          </div>
+          <MdOutlineAddHomeWork size={40} style={{color: '#3A5295'}} />
+        </div>
+        
         <form>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="tel" placeholder="Phone Number" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Submit</button>
+          <div className='form-container'>
+            <div className='personal-info-conatiner'>
+              <div className='personal-info'>
+                <label htmlFor='name'>Name</label>
+                <input name='name' type="text" placeholder="Enter your name" />
+              </div>
+
+              <div className='personal-info'>
+                <label htmlFor='email'>Email</label>
+                <input name='email' type="email" placeholder=" Enter your email" />
+              </div>
+
+              <div className='personal-info'>
+                <label htmlFor='phoneNumber'>Phone Number</label>
+                <input name='phoneNumber' type="tel" placeholder="Enter your phone number" />
+              </div>
+            </div>
+
+            <div className='message-container'>
+              <div className='personal-info'>
+                <label htmlFor='message'>Message</label>
+                <textarea name='message' placeholder="Message"></textarea>
+              </div>
+              <button className='submit-button' type="submit">Submit</button>
+            </div>
+          </div>
+
         </form>
       </div>
     </div>
