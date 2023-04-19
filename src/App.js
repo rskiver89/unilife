3c4b6bf2136fcd6235190655d6e51b10ae09f52f
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-04-19 12:21:49 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import axios from 'axios';
 import SeeAllCities from './pages/SeeAllCities/SeeAllCities';
-import CityDetails from './pages/CityDetails';
+import CityDetails from './pages/CityDetails/CityDetails';
 import HomeDetails from './pages/HomeDetails';
 
 function App() {
--- src/pages/CityDetails/CityDetails.js
+++ src/pages/CityDetails/CityDetails.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import PropertyFilter from '../components/PropertyFilter/PropertyFilter';
-import Banner from '../components/Banner/Banner';
-import '../styles/CityDetails.css';
+import PropertyFilter from '../../components/PropertyFilter/PropertyFilter';
+import Banner from '../../components/Banner/Banner';
+import './CityDetails.css';
 import { Link, useParams } from 'react-router-dom';
 import { MdOutlineBedroomParent, MdOutlineBathtub, MdHome } from 'react-icons/md';
 import { IoLocationOutline } from 'react-icons/io5';
-import Students from '../assets/student-details.png';
+import Students from '../../assets/student-details.png';
 
 function CityDetails({ cityDetails }) {
   const { city } = useParams();
@@ -37,7 +37,7 @@ function CityDetails({ cityDetails }) {
 
     // Filter properties
     const cityProperties = allProperties.filter(
-      (property) => property.address.city.toLowerCase() === city.toLowerCase()
+      (property) => property?.address?.city.toLowerCase() === city.toLowerCase()
     );
 
     setProperties(cityProperties);
@@ -48,9 +48,9 @@ function CityDetails({ cityDetails }) {
       <Banner mainText='Search Accomodation' subText='Whatever you’re after, we can help you find the right student accommodation for you. ' />
 
       <PropertyFilter />
-
       <div className='properties-wrapper'>
-        <h1 style={{ textAlign: 'center', margin: '12px' }}>Property Details</h1>
+      <h2 className='homes-in-city'>{properties.length} Homes in {currentCity?.name}</h2>
+
 
         <div className='properties-container'>
           {properties.map((property) => (
@@ -81,16 +81,16 @@ function CityDetails({ cityDetails }) {
                   <div className='location'>
                     <IoLocationOutline />
                     <address>
-                      {property?.address.street}, {property?.address.city}, {property?.address.postcode}
+                      {property?.address?.street}, {property?.address?.city}, {property?.address?.postcode}
                     </address>
                   </div>
                 </div>
-                <div className='view-home'>
-                  <Link to={`/home-details/${property._id}`} style={{ textDecoration: 'none' }}>
-                    <h4>
-                      {' '}
-                      <MdHome /> View Home
-                    </h4>
+                <div className='view-home-container'>
+                  <Link to={`/home-details/${property?._id}`} style={{ textDecoration: 'none' }}>
+                    <div className='view-home'>
+                    <MdHome size={20}/>
+                    <h4>View Home</h4>
+                    </div>
                   </Link>
                 </div>
               </div>
@@ -99,18 +99,19 @@ function CityDetails({ cityDetails }) {
         ))}
       </div>
     </div>
-
+<div className='student-wrapper'>
     <div className='student-container'>
       <div className='student-info'>
         {currentCity && (
           <>
-            <h2>Being a student in {currentCity.name}</h2>
-            <p>{currentCity.student_life}</p>
-            <p>{currentCity.universities}</p>
+            <h2 style={{fontWeight: '500'}}>Being a student in {currentCity?.name}</h2>
+            <p>{currentCity?.student_life}</p>
+            <p>{currentCity?.universities}</p>
           </>
         )}
       </div>
       <img src={Students} style={{ width: '40%' }} />
+    </div>
     </div>
   </div>
 );
